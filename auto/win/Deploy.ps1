11bea1a3155e26ab85e7b1cccd39d51b2a5c2534
Peter Berry <pwcberry@gmail.com>
2025-02-17 22:20:40 +1100

    WIP

---
+++ auto/win/Deploy.ps1
@@ -0,0 +1,3 @@
+# See: https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_scripts?view=powershell-7.5
+# See: https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-7.5
+Write-Host "Hello World"
